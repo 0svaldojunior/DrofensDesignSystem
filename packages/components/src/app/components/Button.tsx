2a37848fa994037064d06f4ec247a37884ec54d1
0svaldojunior <app.o.r.jr@gmail.com>
2023-05-29 14:33:31 -0300

    Changeset configured with successfully

--- packages/components/src/app/components/Button.tsx
+++ packages/components/src/app/components/Button.tsx
@@ -4,7 +4,7 @@ export function Button({ children }: ChildrenReactNode) {
   return (
     <div className="">
       <button className="rounded-lg bg-purple-500 px-8 py-3 text-xl font-bold text-gray-50">
-        {children}
+        {children}ola
       </button>
     </div>
   )
---
+++ packages/eslint-config/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @drofens/eslint-config
+
+## 1.0.1
+
+### Patch Changes
+
+- Finished eslint config
