6de84b060aec2d87cd41a0ddf0bbd57f9be75025
0svaldojunior <app.o.r.jr@gmail.com>
2023-05-31 08:02:06 -0300

    add: class dark to background

--- packages/components/src/app/page.tsx
+++ packages/components/src/app/page.tsx
@@ -3,7 +3,9 @@ import { ButtonTheme } from './components/ButtonTheme'
 
 export default function Home() {
   return (
-    <div className={`flex h-screen w-screen flex-col items-center justify-center gap-4`}>
+    <div
+      className={`flex h-screen w-screen flex-col items-center justify-center gap-4 bg-white dark:bg-gray-950`}
+    >
       <Box>
         <ButtonTheme />
       </Box>
